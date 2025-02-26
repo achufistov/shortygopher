d20631e6410baba5c215a5f248f6f4fcfedb2feb
achufistov <achufistov6@gmail.com>
2025-02-26 08:28:41 +0300

    add unit tests for get/post handlers

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,84 @@
 package main
 
-func main() {}
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var (
+	urlMap = make(map[string]string)
+)
+
+func main() {
+	http.HandleFunc("/", handlePost)
+	http.HandleFunc("/{id}", handleGet)
+
+	log.Println("Server is running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handlePost(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusBadRequest)
+		return
+	}
+
+	originalURL, err := readBody(r)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	shortURL := generateShortURL()
+	urlMap[shortURL] = originalURL
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "http://localhost:8080/%s", shortURL)
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid request method", http.StatusBadRequest)
+		return
+	}
+
+	id := r.URL.Path[1:] // Remove the leading '/'
+	originalURL, exists := urlMap[id]
+
+	if !exists {
+		http.Error(w, "URL not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Location", originalURL)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func readBody(r *http.Request) (string, error) {
+	if !strings.Contains(r.Header.Get("Content-Type"), "text/plain") {
+		return "", errors.New("invalid content type")
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
+func generateShortURL() string {
+	b := make([]byte, 6)
+	_, err := rand.Read(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return base64.URLEncoding.EncodeToString(b)[:6]
+}
